3e05fb3594deb1fe91a04a0b4aaff1eb75fad477
DELL <rakeshdr543@gmail.com>
2021-01-13 15:06:16 +0530

    Added router ssr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import {useDispatch, useSelector} from "react-redux";
+import {Route, Switch, NavLink, BrowserRouter} from 'react-router-dom';
+import Component from "./Component1";
 
 function App() {
   const app = useSelector((state)=>state.app)
@@ -15,11 +17,22 @@ function App() {
 
   };
   return (
-      <>
+      <BrowserRouter>
         <p>{count}</p>
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
-      </>
+          <hr />
+
+          <h2>Part 3: React router</h2>
+          <nav>
+              <NavLink to="/" exact activeClassName="active">Home</NavLink>
+              <NavLink to="/another" activeClassName="active">Another page</NavLink>
+          </nav>
+          <Switch>
+              <Route path="/" exact component={Component} />
+              <Route path="/another" component={Component} />
+          </Switch>
+      </BrowserRouter>
   );
 }
 
---
+++ src/Component1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default  function Component() {
+    return (
+        <h2>First component</h2>
+    )
+}
